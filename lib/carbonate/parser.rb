732efd7272dfba827b8a3976d0736340bfe10acd
Vsevolod Romashov <7@7vn.ru>
2015-09-20 23:14:37 +0300

    defclass, defmethod & def constructs

--- lib/carbonate/parser.rb
+++ lib/carbonate/parser.rb
@@ -4,19 +4,49 @@ require 'parser/ast/node'
 
 module Carbonate
   class Parser < Rly::Yacc
+    def without_spaces(elements)
+      elements.reject do |arg|
+        arg.type == :S
+      end
+    end
+
     def s(type, children)
+      self.class.s(type, children)
+    end
+
+    def self.s(type, children)
       ::Parser::AST::Node.new(type, children)
     end
 
     lexer do
-      literals '+-*/()'
+      literals '+-*/()[]'
 
       token :NUMBER, /\d+/ do |t|
-        t.value = t.value.to_i
+        t.value = Parser.s(:int, [t.value.to_i])
         t
       end
 
-      token :WHITESPACE, /\s+/
+      # whitespace
+      token :S, /\s+/
+
+      token :DEFCLASS,  /defclass/
+      token :DEFMETHOD, /defmethod/
+      token :DEF,       /def/
+
+      token :CONST, /[A-Z][A-Za-z0-9]*/ do |t|
+        t.value = Parser.s(:const, [nil, t.value.to_sym])
+        t
+      end
+
+      token :LVAR, /[a-z][A-Za-z0-9_-]*/ do |t|
+        t.value = t.value.gsub(/-/, '_').to_sym
+        t
+      end
+
+      token :IVAR, /@[a-z][A-Za-z0-9_-]*/ do |t|
+        t.value = t.value.gsub(/-/, '_').to_sym
+        t
+      end
 
       on_error do |t|
         puts "Illegal character #{t.value.inspect} at line #{t.lexer.lineno + 1}"
@@ -25,26 +55,78 @@ module Carbonate
       end
     end
 
-    rule 'sexp : "(" func WHITESPACE args ")"' do |sexp, _, func, _, args, _|
-      sexp.value = s(:send, [args.value.first, func.value, *args.value[1..-1]])
+    # outermost scope
+    rule 'source : sexps S | sexps' do |source, sexps|
+      source.value = s(:begin, sexps.value)
     end
 
+    # multiple S-expressions
+    rule 'sexps : sexps S sexp | sexp' do |sexps, *sexps_array|
+      sexps.value = without_spaces(sexps_array).flat_map(&:value)
+    end
+
+    # function/method call
+    # (+ 2 2)
+    rule 'sexp : "(" func S forms ")"' do |sexp, _, func, _, forms, _|
+      sexp.value = s(:send, [forms.value.first, func.value, *forms.value[1..-1]])
+    end
+
+    # class definition
+    # (defclass User (class body))
+    rule 'sexp : "(" DEFCLASS S CONST S sexps ")"' do |sexp, _, _, _, const, _, sexps, _|
+      sexp.value = s(:class, [const.value, nil, *sexps.value])
+    end
+
+    # method defition
+    # (defmethod full-name (join [first-name last-name]))
+    rule 'sexp : "(" DEFMETHOD S LVAR S arguments_list S sexps ")"' do |sexp, _, _, _, method_name, _, args, _, sexps, _|
+      method_body = s(:begin, sexps.value)
+      sexp.value = s(:def, [method_name.value.to_sym, args.value, method_body])
+    end
+
+    # assignment
+    # (def age 30)
+    rule 'sexp : "(" DEF S IVAR S form ")"' do |sexp, _, _, _, var_name, _, form, _|
+      sexp.value = s(:ivasgn, [var_name.value, form.value])
+    end
+
+    # multiple forms
+    rule 'forms : forms S form | form' do |forms, *forms_array|
+      forms.value = without_spaces(forms_array).flat_map(&:value)
+    end
+
+    # form can be a local variable
+    rule 'form : LVAR' do |form, lvar|
+      form.value = s(:lvar, [lvar.value])
+    end
+
+    # form can also be an instance variable, a number or an S-expression
+    rule 'form : IVAR | NUMBER | sexp' do |form, element|
+      form.value = element.value
+    end
+
+    # method arguments list (in method definition)
+    # [a b c]
+    rule 'arguments_list : "[" arguments "]"' do |arguments_list, _, arguments, _|
+      arguments_list.value = s(:args, arguments.value)
+    end
+
+    # multiple arguments
+    rule 'arguments : arguments S argument | argument | empty' do |arguments, *arguments_array|
+      arguments.value = without_spaces(arguments_array).flat_map(&:value)
+    end
+
+    # an argument can be a local variable
+    rule 'argument : LVAR' do |argument, identifier|
+      argument.value = s(:arg, [identifier.value])
+    end
+
+    # empty rule
+    rule('empty :') {}
+
+    # function/method (may be operator)
     rule 'func : "+" | "-" | "*" | "/"' do |func, operator|
       func.value = operator.value.to_sym
-    end
-
-    rule 'args : args WHITESPACE arg | arg' do |args, *args_array|
-      args.value = args_array.reject do |arg|
-        arg.type == :WHITESPACE
-      end.map(&:value).flatten
-    end
-
-    rule 'arg : NUMBER' do |arg, number|
-      arg.value = s(:int, [number.value])
-    end
-
-    rule 'arg : sexp' do |arg, sexp|
-      arg.value = sexp.value
     end
   end
 end
--- spec/carbonate/parser_spec.rb
+++ spec/carbonate/parser_spec.rb
@@ -5,21 +5,67 @@ RSpec.describe Carbonate::Parser do
     Parser::AST::Node.new(type, children)
   end
 
-  let(:source) { '(+ 2 (* 3 4))' }
+  context 'with basic arithmetic' do
+    let(:basic)  { '(+ 2 2)' }
+    let(:nested) { '(+ 2 (* 3 4))' }
 
-  it 'parses the source into AST' do
-    expect(subject.parse(source)).to eq(
-      s(
-        :send,
-        s(:int, 2),
-        :+,
-        s(
-          :send,
-          s(:int, 3),
-          :*,
-          s(:int, 4)
+    it 'parses the source into AST' do
+      expect(subject.parse(basic)).to eq(
+        s(:begin,
+          s(:send,
+            s(:int, 2),
+            :+,
+            s(:int, 2)
+          )
         )
       )
-    )
+
+      expect(subject.parse(nested)).to eq(
+        s(:begin,
+          s(:send,
+            s(:int, 2),
+            :+,
+            s(:send,
+              s(:int, 3),
+              :*,
+              s(:int, 4)
+            )
+          )
+        )
+      )
+    end
+  end
+
+  context 'with a class definition' do
+    let(:source) do
+      <<-CRB
+(defclass User
+  (defmethod initialize [first-name last-name]
+    (def @first-name first-name)
+    (def @last-name last-name)))
+      CRB
+    end
+
+    it 'parses the source into AST' do
+      expect(subject.parse(source)).to eq(
+        s(:begin,
+          s(:class,
+            s(:const, nil, :User),
+            nil,
+            s(:def,
+              :initialize,
+              s(:args,
+                s(:arg, :first_name),
+                s(:arg, :last_name)
+              ),
+              s(:begin,
+                s(:ivasgn, :@first_name, s(:lvar, :first_name)),
+                s(:ivasgn, :@last_name, s(:lvar, :last_name))
+              )
+            )
+          )
+        )
+      )
+    end
   end
 end
