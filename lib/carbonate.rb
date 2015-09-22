a7fc4e83e0abdbfbf725e334706bb603942e53dd
Vsevolod Romashov <7@7vn.ru>
2015-09-23 00:32:14 +0300

    Carbonate.process

--- lib/carbonate.rb
+++ lib/carbonate.rb
@@ -1,6 +1,21 @@
 require 'carbonate/parser'
+require 'unparser'
 require 'carbonate/version'
 
 module Carbonate
-  # Your code goes here...
+  class << self
+    def process(source)
+      ast = Parser.new.parse(source)
+      with_trailing_newline(Unparser.unparse(ast))
+    end
+
+  private
+    def with_trailing_newline(text)
+      if text.end_with?(?\n)
+        text
+      else
+        text + ?\n
+      end
+    end
+  end
 end
--- spec/carbonate_spec.rb
+++ spec/carbonate_spec.rb
@@ -4,4 +4,42 @@ describe Carbonate do
   it 'has a version number' do
     expect(Carbonate::VERSION).not_to be nil
   end
+
+  describe '.process' do
+    let(:source) do
+      <<-CRB
+(defclass User
+  (defmethod initialize [first-name last-name age]
+    (def @first-name first-name)
+    (def @last-name last-name)
+    (def @age age))
+  (defmethod full-name []
+    (join [@first-name @last-name]))
+  (defmethod age []
+    (to-i @age)))
+      CRB
+    end
+
+    let(:result) do
+      <<-RB
+class User
+  def initialize(first_name, last_name, age)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+  end
+  def full_name
+    [@first_name, @last_name].join
+  end
+  def age
+    @age.to_i
+  end
+end
+      RB
+    end
+
+    it 'takes a carbonate source and returns a ruby source' do
+      expect(subject.process(source)).to eq(result)
+    end
+  end
 end
